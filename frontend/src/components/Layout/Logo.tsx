9b1afdedcb448673e68ed3c1b7d341b6ddec570b
rsln-dev <147005298+rsln-dev@users.noreply.github.com>
2025-08-16 18:04:36 -0700

    frontend added homepage

--- frontend/src/components/Layout/Logo.tsx
+++ frontend/src/components/Layout/Logo.tsx
@@ -1,5 +1,10 @@
 import { Box } from '@mantine/core';
+import { Link } from 'react-router-dom';
 
 export const Logo = () => {
-	return <Box>russchords</Box>;
+	return (
+		<Box component={Link} to={'/'} text-decoration="none" style={{ all: 'unset', cursor: 'pointer' }}>
+			russchords
+		</Box>
+	);
 };
